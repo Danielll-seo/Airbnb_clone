16386c306f22f967a976937f805ebccdc43d6dd0
Daniel_sxxnt <shj1103s@naver.com>
2024-07-02 09:05:48 +0900

    add house model

--- Backend/houses/admin.py
+++ Backend/houses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from houses.models import House
 
 # Register your models here.
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    pass
--- Backend/houses/models.py
+++ Backend/houses/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+""" House model """
 class House(models.Model):
 
     name = models.CharField(max_length=140)
     price = models.PositiveBigIntegerField()
-    
+    description = models.TextField()
+    address = models.CharField(max_length=140)
