74fc3293eeed8ff40d65b77fec2c4e73a4a3f8d8
Daniel_sxxnt <shj1103s@naver.com>
2024-07-02 12:11:41 +0900

    fix

--- Backend/houses/models.py
+++ Backend/houses/models.py
@@ -4,10 +4,14 @@ from django.db import models
 class House(models.Model):
 
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveBigIntegerField()
+    price_per_night = models.PositiveBigIntegerField(verbose_name="Price", help_text="positive number only")
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True)
+    pets_allowed = models.BooleanField(
+        verbose_name="Pets allowed?",
+        default=True,
+        help_text="Does this house allows pets?"
+    )
 
     def __str__(self):
         return self.name
