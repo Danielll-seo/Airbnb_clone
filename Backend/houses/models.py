c2588cd4c2f7dab2d0b115ceeaebfc79f621bb45
Daniel_sxxnt <shj1103s@naver.com>
2024-07-15 11:39:22 +0900

    update

--- Backend/houses/models.py
+++ Backend/houses/models.py
@@ -12,6 +12,7 @@ class House(models.Model):
         default=True,
         help_text="Does this house allows pets?"
     )
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- Backend/users/models.py
+++ Backend/users/models.py
@@ -3,7 +3,21 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150, editable=False)
-    last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150, default="")
-    is_host = models.BooleanField(default=False)
+    first_name = models.CharField(
+        max_length=150,
+        editable=False
+    )
+
+    last_name = models.CharField(
+        max_length=150,
+        editable=False
+    )
+
+    name = models.CharField(
+        max_length=150,
+        default=""
+    )
+
+    is_host = models.BooleanField(
+        default=False
+    )
