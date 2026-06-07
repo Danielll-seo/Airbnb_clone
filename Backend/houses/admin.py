e555c772f354ed9c556085568ae6fea3bcb6b0b0
Daniellll <shj1103s@naver.com>
2026-06-07 13:55:50 +0000

    Houses model에 대한 구조개편, 검색기능 추가

--- Backend/houses/admin.py
+++ Backend/houses/admin.py
@@ -3,4 +3,21 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
+    
+    """Admin Definition for House"""
+
+    list_display = (
+        "name",
+        "price_per_night",
+        "address",
+        "pet_allowed",
+    )
+
+    list_filter = [
+        "price_per_night",
+        "pet_allowed"
+    ]
+
+    search_fields = [
+        "name", "address"
+    ]
--- Backend/houses/models.py
+++ Backend/houses/models.py
@@ -5,6 +5,17 @@ class House(models.Model):
     """Model Definition for Houses"""
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price",
+        help_text="Price per night in KRW"
+    )
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pet_allowed = models.BooleanField(
+        verbose_name="Pet Allowed?",
+        default=True,
+        help_text="Is pet allowed in this house?"
+    )
+
+    def __str__(self):
+        return self.name
