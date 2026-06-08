36b19b30fe285ba65b09ca4e577d4fc628bfa5e2
Daniellll <shj1103s@naver.com>
2026-06-08 13:56:07 +0000

    사용자 모델에 필드 추가: first_name, last_name, name, is_host

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -3,4 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150)
+    is_host = models.BooleanField()
