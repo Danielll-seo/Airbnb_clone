f7937604934b1b3117647462abfafe07092a22d0
Daniellll <shj1103s@naver.com>
2026-06-24 13:07:32 +0000

    Updated verbose name of DMS

--- Backend/direct_messages/apps.py
+++ Backend/direct_messages/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class DirectMessagesConfig(AppConfig):
+    default_auto_filed = "django.db.models.BigAutoField"
+    verbose_name = "Direct Messages"
     name = "direct_messages"
