e341c125c4931ebeb1428b1d26aeacf0e4494388
Daniellll <shj1103s@naver.com>
2026-07-05 14:25:19 +0000

    URL 구성 및 뷰 추가

--- Backend/rooms/views.py
+++ Backend/rooms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def see_all_rooms(request):
+    return HttpResponse("see all rooms!")
+
+def see_one_room(request):
+    return HttpResponse("see a room")
