f8fd09b0d1ae48fb9499e37da1b9d23843e78324
Daniellll <shj1103s@naver.com>
2026-07-07 14:41:59 +0000

    Json 안쓰고 Rest api_view씀

--- Backend/categories/views.py
+++ Backend/categories/views.py
@@ -1,6 +1,11 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 
+@api_view()
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse({'ok': True, 'categories': all_categories})
+    return Response(
+        {
+            'ok': True,
+        }
+    )
